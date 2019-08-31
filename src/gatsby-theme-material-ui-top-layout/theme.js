9e961e67498baf8645bfb717d2ad08114689dc4b
zodiacfireworks <martin.vuelta@gmail.com>
2019-08-31 03:12:47 -0500

    Updates

--- src/gatsby-theme-material-ui-top-layout/theme.js
+++
@@ -1,9 +0,0 @@
-import { createMuiTheme } from "@material-ui/core"
-
-const theme = createMuiTheme({
-  typography: {
-    fontFamily: ["Fira Sans", "sans-serif"].join(","),
-  },
-})
-
-export default theme
